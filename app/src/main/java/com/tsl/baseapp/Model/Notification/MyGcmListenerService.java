f586b31da293c4a02150cb70c9e34f4804956678
Kevin Lavi <kl@tsl.io>
2016-05-06 14:41:18 -0400

    BA-174 Cropping Image

--- app/src/main/java/com/tsl/baseapp/Model/Notification/MyGcmListenerService.java
+++ app/src/main/java/com/tsl/baseapp/Model/Notification/MyGcmListenerService.java
@@ -11,8 +11,8 @@ import android.support.v4.app.NotificationCompat;
 
 
 import com.google.android.gms.gcm.GcmListenerService;
-import com.tsl.baseapp.Activities.MainActivity;
 import com.tsl.baseapp.R;
+import com.tsl.baseapp.feed.FeedActivity;
 
 /**
  * Created by kevinlavi on 3/3/16.
@@ -55,7 +55,7 @@ public class MyGcmListenerService extends GcmListenerService {
          * @param message GCM message received.
          */
         private void sendNotification(String message) {
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, FeedActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                     PendingIntent.FLAG_ONE_SHOT);
